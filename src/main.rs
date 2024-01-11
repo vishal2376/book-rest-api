ffb4730d20dbcecd5e79450128fa23eb216e71fd
vishal2376 <vishalsingh2376@gmail.com>
2024-01-11 16:55:10 +0530

    chore: add post and patch service

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,29 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, patch, post, App, HttpResponse, HttpServer, Responder};
 
 #[get("/books")]
 async fn get_books() -> impl Responder {
     HttpResponse::Ok().body("Books available")
 }
 
+#[post("/books")]
+async fn add_book() -> impl Responder {
+    HttpResponse::Ok().body("Book Added")
+}
+
+#[patch("/books/{uuid}")]
+async fn update_book() -> impl Responder {
+    HttpResponse::Ok().body("Book Updated")
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(get_books))
-        .bind("127.0.0.1:8080")?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .service(get_books)
+            .service(add_book)
+            .service(update_book)
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
