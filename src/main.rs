790abf9cdb2d79b8789d826a6d5b24e11d653e2f
vishal2376 <vishalsingh2376@gmail.com>
2024-01-11 16:40:02 +0530

    chore: setup server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+#[get("/books")]
+async fn get_books() -> impl Responder {
+    HttpResponse::Ok().body("Books available")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(get_books))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
